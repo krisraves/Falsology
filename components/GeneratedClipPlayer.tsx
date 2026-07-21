3f3173a50e1f6734787665a25f0f4a36ddaa0a35
Kris <krisraves@gmail.com>
2026-07-21 09:07:26 -0500

    Size generated video to the evidence frame

--- components/GeneratedClipPlayer.tsx
+++ components/GeneratedClipPlayer.tsx
@@ -66,6 +66,7 @@ export function GeneratedClipPlayer({
         <video
           ref={videoRef}
           className="generated-clip-video"
+          style={{ width: "100%", height: "100%", objectFit: "cover", display: "block", background: "#000" }}
           src={src}
           preload="metadata"
           playsInline
